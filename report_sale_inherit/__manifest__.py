d91939918a027ca0498136af2e8741bbcdca31ad
Juan Pablo Calderon Gallardo <jcalderon@kuh7.mx>
2022-01-30 23:20:11 -0600

    cotizacion

--- report_sale_inherit/__manifest__.py
+++ report_sale_inherit/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Reporte de cotizacion modificado',
-    'summary': 'Cambios en el reporte',
+    'summary': 'Cambio en el reporte',
     'description': '''
-    * Cambia las 4 decimales a 2 decimales.
+    * El precio del producto tiene 4 decimales y cambia 2 decimales.
     ''',
     "website": "https://www.kuh7.mx",
     'author': 'KUH7 SOLUCIONES S.A. DE C.V.',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'sale',
     'depends': [
         'sale',
